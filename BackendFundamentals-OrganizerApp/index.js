bcfad8e8be9a01efd194ccb9a03bd763620696b9
luchivanelli <lucianavanelli13@gmail.com>
2022-05-01 19:19:00 -0300

    Updated

--- BackendFundamentals-OrganizerApp/index.js
+++ BackendFundamentals-OrganizerApp/index.js
@@ -4,13 +4,21 @@ const path = require("path")  //permite administrar rutas
 const express = require("express")
 const port = 4000
 
+//Importando router
+const users = require("./routes/users") // También podemos usar: require("./routes/users.js")
+
 const app = express()
 
+//Sección para los middleware
+app.use("/static",express.static(path.join(__dirname,"static"))) //Middleware para archivos estaticos
+
+// Sección de codigo para los router
+app.use(users) // Usando un router
 
 //req= peticion , res= respuesta
 app.get("/",function(req,res) {
     console.log(__dirname) //Ruta de nuestro proyecto (raiz)
-    return res.sendFile(path.join(__dirname,"static","index.html"))
+    return res.sendFile(path.join(__dirname,"views","index.html"))
 })
 
 app.listen(port,()=>{
