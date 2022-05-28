6b32b8e30687c8b71a2db9d1560dd4f5c5ee5a16
luchivanelli <lucianavanelli13@gmail.com>
2022-05-28 14:45:43 -0300

    Updated

---
+++ CodingFundamentals-Juego/index.js
@@ -0,0 +1,57 @@
+colorContainer = document.querySelector(".color-container");
+mainContainer = document.querySelector(".main-container")
+redButton = document.querySelector(".red-button");
+blueButton = document.querySelector(".blue-button");
+greenButton = document.querySelector(".green-button");
+finishButton = document.querySelector(".finish-button")
+
+correctButton1 = document.querySelector(".button-3")
+correctButton2 = document.querySelector(".button-4")
+correctButton3 = document.querySelector(".button-8")
+correctButton4 = document.querySelector(".button-12")
+correctButton5 = document.querySelector(".button-14")
+
+counter = 0;
+
+redButton.addEventListener('click', ()=> {
+    colorContainer.style.border = "4px solid red";
+    mainContainer.style.border = "4px solid red";
+    finishButton.style.background = "red"
+})
+
+blueButton.addEventListener('click', ()=> {
+    colorContainer.style.border = "4px solid blue";
+    mainContainer.style.border = "4px solid blue";
+    finishButton.style.background = "blue"
+})
+
+greenButton.addEventListener('click', ()=> {
+    colorContainer.style.border = "4px solid green";
+    mainContainer.style.border = "4px solid green";
+    finishButton.style.background = "green"
+})
+
+correctButton1.addEventListener("click", ()=> {
+    counter += 20;
+})
+
+correctButton2.addEventListener("click", ()=> {
+    counter += 20;
+})
+
+correctButton3.addEventListener("click", ()=> {
+    counter += 20;
+})
+
+correctButton4.addEventListener("click", ()=> {
+    counter += 20;
+})
+
+correctButton5.addEventListener("click", ()=> {
+    counter += 20;
+})
+
+finishButton.addEventListener('click', ()=> {
+    alert(`Su puntaje total es de: ${counter} puntos`);
+})
+
